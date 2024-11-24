c9e6f5c8e4edb1e335d34618bce920fcbb40d6d3
Fimil Faneea <fimilfaneea@gmail.com>
2024-11-24 06:41:40 +0530

    documentation added

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ void main() {
   final subjectRepository = SubjectRepository(fetchSubjectsService);
   final fetchModulesService = FetchModulesService();
 
+  // Run the app with necessary services and repositories
   runApp(MyApp(
     fetchSubjectsService: fetchSubjectsService,
     subjectRepository: subjectRepository,
@@ -23,11 +24,21 @@ void main() {
   ));
 }
 
+/// Main application widget that initializes necessary services and repositories.
+///
+/// This widget is the entry point for the application and sets up providers for the 
+/// necessary services, repositories, and Blocs. It also defines the routes for the app.
 class MyApp extends StatelessWidget {
+  /// Service to fetch subjects.
   final FetchSubjectsService fetchSubjectsService;
+
+  /// Repository for handling subjects.
   final SubjectRepository subjectRepository;
+
+  /// Service to fetch modules.
   final FetchModulesService fetchModulesService;
 
+  /// Constructor that accepts the required services and repositories.
   const MyApp({
     super.key,
     required this.fetchSubjectsService,
@@ -39,7 +50,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        // Provide the repositories
+        // Provide the repositories for fetching subjects and modules
         RepositoryProvider<FetchSubjectsService>(
           create: (_) => fetchSubjectsService,
         ),
@@ -49,7 +60,7 @@ class MyApp extends StatelessWidget {
         RepositoryProvider<FetchModulesService>(
           create: (_) => fetchModulesService,
         ),
-        // Provide the Blocs
+        // Provide the Blocs that will manage the states of subjects and modules
         BlocProvider<SubjectBloc>(
           create: (_) => SubjectBloc(subjectRepository),
         ),
@@ -60,15 +71,18 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'LMS',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const SubjectsScreen(),
+        home: const SubjectsScreen(), // Initial screen when the app loads
         routes: {
+          // Define the routes for the app navigation
           '/videos': (context) => const VideosPage(),
           '/modules': (context) {
+            // Extract arguments passed to the modules screen
             final arguments = ModalRoute.of(context)?.settings.arguments
                 as Map<String, dynamic>;
             final subjectId = arguments['subjectId'] as String;
             final subjectTitle = arguments['subjectTitle'] as String;
 
+            // Return the ModulesPage with the subject details
             return ModulesPage(
                 subjectId: subjectId, subjectTitle: subjectTitle);
           },
